05ff074ab8ab62143f91ac800f7e8953e9dd5a12
Stephane Poirier <stephane.poirier@oifii.org>
2016-12-12 20:10:46 -0500

    license and readme

--- main2.c
+++ main2.c
@@ -1,11 +1,26 @@
-// sample_multithread_c_program.c
-// compile with: /c
-//
-//  Bounce - Creates a new thread each time the letter 'a' is typed.
-//  Each thread bounces a happy face of a different color around
-//  the screen. All threads are terminated when the letter 'Q' is
-//  entered.
-//
+/*
+ * Copyright (c) 2010-2016 Stephane Poirier
+ *
+ * stephane.poirier@oifii.org
+ *
+ * Stephane Poirier
+ * 3532 rue Ste-Famille, #3
+ * Montreal, QC, H2X 2L1
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
 #include <windows.h>
 #include <stdlib.h>
